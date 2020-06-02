b3c36189bd1bee342a9694100c60359b627f95b6
Jawad Ahmed <jawad-ahmed23@users.noreply.github.com>
2020-06-02 15:34:31 +0500

    Updates

---
+++ precache-manifest.d3debaffe8e971df4a6ac458e02504b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3fb3bd97451a622a85e31120be45e5",
+    "url": "/robo-app/index.html"
+  },
+  {
+    "revision": "90c866cd5d0d3ad3888d",
+    "url": "/robo-app/static/css/main.c86d252e.chunk.css"
+  },
+  {
+    "revision": "d4c17f5751945131b11a",
+    "url": "/robo-app/static/js/2.b3593daf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-app/static/js/2.b3593daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c866cd5d0d3ad3888d",
+    "url": "/robo-app/static/js/main.8276f257.chunk.js"
+  },
+  {
+    "revision": "96ffc463f5135216d3ee",
+    "url": "/robo-app/static/js/runtime-main.d77687c5.js"
+  }
+]);
